6b5e4c0b8bb1862e34cc8ed4962bd13738ea9f10
sugiwa <sugi.warra@gmail.com>
2022-04-29 23:56:27 +0900

    add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped right>
+      Navi
+    </v-navigation-drawer>
+    <v-app-bar color="primary" app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+
+    <v-footer color="primary" app>
+      Vuetify  
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    drawer: null
+  }),
+}
+</script>
