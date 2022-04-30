e788f9c4cb81359175feabb6c4271a544b9c43dd
sugiwa <sugi.warra@gmail.com>
2022-05-01 08:37:25 +0900

    add user index

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { loadFonts } from './plugins/webfontloader'
+
+import UserIndex from './components/UserIndex.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 loadFonts()
 
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/users', component: UserIndex }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
 createApp(App)
   .use(vuetify)
+  .use(router)
   .mount('#app')
