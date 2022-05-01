be888148dd52fe06b21694c8e011e1e791a758d6
sugiwa <sugi.warra@gmail.com>
2022-05-01 15:23:24 +0900

    add feature show user and create user

--- spring_api/src/main/java/com/example/demo/db_test/UserController.java
+++ spring_api/src/main/java/com/example/demo/db_test/UserController.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/users")
 public class UserController {
 
 	@Autowired
 	UserRepository repository;
 	
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public List<User> getUsers() {
 		
 		List<User> usersList = new ArrayList<User>();
@@ -27,20 +29,42 @@ public class UserController {
 		return usersList;
 	}
 
-	@RequestMapping(value = "/test_users", method = RequestMethod.GET)
-	public List<User> getTestUsers() {
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	public User getUser(@PathVariable int userId) {
+		User user = new User();
 		
-		List<User> usersList = new ArrayList<User>();
+		user = repository.getById(userId);
 		
-		usersList.add(new User());
-		usersList.add(new User());
+		return user;
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void createUser(@RequestBody UserForm form) {
+		System.out.println(form);
 		
-		return usersList;
+		User user = new User();
+		user.setName(form.getName());
+		user.setEmail(form.getEmail());
+		user.setPassword(form.getPassword());
+		repository.save(user);
+		
+		
+		
 	}
 	
 	@RequestMapping(value = "/test")
 	public String getTest() {
 		return "OK!";
+	}
+	
+	@RequestMapping(value = "/testUser", method = RequestMethod.GET)
+	public List<User> getTestUsers() {
+		
+		List<User> usersList = new ArrayList<User>();
+		
+		usersList.add(new User());
+		
+		return usersList;
 	}
 	
 
