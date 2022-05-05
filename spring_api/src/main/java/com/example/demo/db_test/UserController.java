7d98ef5a7efcb47215996b2559ebfeb911b25585
sugiwa <sugi.warra@gmail.com>
2022-05-05 22:31:53 +0900

    fix Dockerfile

--- spring_api/src/main/java/com/example/demo/db_test/UserController.java
+++ spring_api/src/main/java/com/example/demo/db_test/UserController.java
@@ -19,10 +19,8 @@ public class UserController {
 	UserRepository repository;
 	
 	@RequestMapping(value = "/api/users", method = RequestMethod.GET)
-	public List<User> getUsers() {
-		
+	public List<User> getAllUsers() {
 		List<User> usersList = new ArrayList<User>();
-		
 		usersList = repository.findAll();
 		
 		return usersList;
@@ -36,36 +34,42 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/api/users/", method = RequestMethod.POST)
-	public void createUser(@RequestBody UserForm form) {
-		System.out.println(form);
-		
+	public void createUser(@RequestBody UserForm form) {		
 		User user = new User();
 		user.setName(form.getName());
 		user.setEmail(form.getEmail());
 		user.setPassword(form.getPassword());
 		repository.save(user);
-		
-		
-		
 	}
 	
+	@RequestMapping(value = "/api/users/{userId}", method = RequestMethod.POST)
+	public void updateUser(@RequestBody UserForm form, @PathVariable int userId) {		
+		User user = repository.getById(userId);
+		user.setName(form.getName());
+		user.setEmail(form.getEmail());
+		user.setPassword(form.getPassword());
+		repository.save(user);
+	}
+
+	@RequestMapping(value = "/api/users/{userId}/delete", method = RequestMethod.DELETE)
+	public void deleteUser(@PathVariable int userId) {
+		User user = repository.getById(userId);
+		repository.delete(user);
+	}
+
+	// TEST
 	@RequestMapping(value = "/api/users/test")
 	public String getTest() {
 		return "OK!";
 	}
-	
 	@RequestMapping(value = "/api/users/valTest/{val}")
 	public int getValTest(@PathVariable int val) {
 		return val;
 	}
-	
 	@RequestMapping(value = "/api/users/testUser", method = RequestMethod.GET)
 	public List<User> getTestUsers() {
-		
 		List<User> usersList = new ArrayList<User>();
-		
 		usersList.add(new User());
-		
 		return usersList;
 	}
 	
