5998e916807e4f753afb5f3b257b03f73d00e3f9
sugiwa <sugi.warra@gmail.com>
2022-05-14 16:19:47 +0900

    delete sample data

--- postgres/initdb/01_init_users.sql
+++ postgres/initdb/01_init_users.sql
@@ -7,9 +7,5 @@ CREATE TABLE users (
 );
 
 -- サンプルレコード作成
-INSERT INTO users (name, email, password) VALUES('jiro', 'a@ahgdy', 'dg3th');
-INSERT INTO users (name, email, password) VALUES('yama', 'alk@b.com', 'hfsar');
-INSERT INTO users (name, email, password) VALUES('satou', 'r@dag.com', 'aaasdg');
-INSERT INTO users (name, email, password) VALUES('souta', 'rdsa@c', 'sgagredg');
 INSERT INTO users (name, email, password) VALUES('osdh', 'asgasd@example.com', 'paordgdh');
 INSERT INTO users (name, email, password) VALUES('rhsosgoi', 'bbb@example.com', 'wsswoghfh');
