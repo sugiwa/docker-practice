2f5b3b06e1a722394af0aa9a24133f30ac9fcfbb
sugiwa <sugi.warra@gmail.com>
2022-05-14 15:46:10 +0900

    fix table: users

--- postgres/initdb/01_init_users.sql
+++ postgres/initdb/01_init_users.sql
@@ -1,10 +1,9 @@
 -- テーブル作成
 CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY(id),
+    id SERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NOT,
-    token VARCHAR(255)
+    password VARCHAR(255) NOT NULL,
 );
 
 -- サンプルレコード作成
