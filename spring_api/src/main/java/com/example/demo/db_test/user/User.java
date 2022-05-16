82e23ba45707310a7cda4bdf70e016cf4503141b
sugiwa <sugi.warra@gmail.com>
2022-05-16 23:29:49 +0900

    change user directory

--- spring_api/src/main/java/com/example/demo/db_test/user/User.java
+++ spring_api/src/main/java/com/example/demo/db_test/user/User.java
@@ -1,44 +1,73 @@
-package com.example.demo.db_test;
+package com.example.demo.db_test.user;
 
-public class UserForm {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "users")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Column
 	private String name;
+	
+	@Column
 	private String email;
+	
+	@Column
 	private String password;
+
+	@Column
 	private String token;
 
 	public String getToken() {
 		return token;
 	}
+
 	public void setToken(String token) {
 		this.token = token;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 	
 }
--- spring_api/src/main/java/com/example/demo/db_test/user/UserRepository.java
+++ spring_api/src/main/java/com/example/demo/db_test/user/UserRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.db_test;
+package com.example.demo.db_test.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
