74384f1131ba08b1fc96efea6bbe158b84eea5a2
sugiwa <sugi.warra@gmail.com>
2022-05-17 00:02:07 +0900

    add schedule view

---
+++ frontend/src/components/Schedule/ScheduleIndex.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="10" class="mx-auto">
+                <h2 class="d-inline mr-10">Users</h2>
+                <v-btn color="success" class="mx-5" @click="openNewSchedule">
+                    Create Schedule
+                </v-btn>
+                <v-table density="compact">
+                    <thead>
+                        <tr>
+                            <th class="text-center">ID</th>
+                            <th class="text-center">Title</th>
+                            <th class="text-center">Content</th>
+                            <th class="text-center">start</th>
+                            <th class="text-center">finish</th>
+                            <th class="text-center"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="schedule in schedules" :key="schedule.id">
+                            <td class="text-center">{{ schedule.id }}</td>
+                            <td class="text-center">{{ schedule.name }}</td>
+                            <td class="text-center">{{ schedule.email }}</td>
+                            <td class="text-center"><v-btn size="small">show</v-btn></td>
+                        </tr>
+                    </tbody>
+                </v-table> 
+            </v-col>
+        </v-row>
+    </v-container>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+const url = 'http://localhost:8080/spring_api/api/'
+
+export default {
+    name: "ScheduleIndex",
+    data: () => ({
+        schedules: [],
+    }),
+    mounted() {
+        axios.get(url + 'schedules')
+            .then((res) => {
+                console.log(res.data)
+                this.schedules = res.data
+            }).catch((err) => {
+                console.log(err)
+            })
+    },
+    methods: {
+        openNewSchedule(){
+            // this.$router.push({ path: '/users/new' })
+        }
+    }
+}
+</script>
